0a1e46d2192a83340f803c0466ccc46a27199e85
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2024-08-11 11:40:43 +0000

    arrow

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,34 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    // Code Block
+    return a + b;
+}
+sum = addTwoNumbers(3, 5);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+sum = addTwoNumbers(3, 5);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Something");
+
+const helloWorld = () => console.log("Hello World!");
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
